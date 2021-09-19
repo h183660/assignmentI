cbe30c3f0c369124192fb581472a01a33f16c09d
h183660 <183660@stud.hvl.no>
2021-09-19 11:47:54 +0200

    .

--- matmul.sh
+++ matmul.sh
@@ -10,10 +10,26 @@ cat /dev/urandom | od | sed "s/\(.\)/\1 /g" | java MatMulASCII
 # Subtask 2A
 case $# in
     2)
-        echo "2 arguments"
+        echo "2 Arguments"
+        if [[ $1 == A*.mat ]] && [[ $2 == B*.mat ]] && [ -f $1 ] && [ -f $2 ]
+        then
+            echo "A*.mat first arg, B*.mat second arg, and both exist in current directory"
+            cat $* | java MatMulASCII
+        elif [[ $1 == B*.mat ]] && [[ $2 == A*.mat ]] && [ -f $1 ] && [ -f $2 ]
+        then
+            echo "B*.mat first arg, A*.mat second arg, and both exist in current directory"
+            cat $2 $1 | java MatMulASCII
+        elif [[ $1 =~ [0-9] ]] && [[ $2 =~ [0-9] ]] && [ -f A$1.mat ] && [ -f B$2.mat ]
+        then
+            echo "Numbers as arguments and files exist in current directory"
+            cat A$1.mat B$2.mat | java MatMulASCII
+        elif [[ $1 == */* ]] && [[ $2 == */* ]]
+        then
+            echo "Files in different directory and they exist"
+        fi
         ;;
     0)
-        echo "0 arguments, Looping through all the valid options for Matrix Multiplication in current directory:"
+        echo "0 Arguments, Looping through all the valid options for Matrix Multiplication in current directory:"
         ls *.mat
         set -- *.mat
         
@@ -23,7 +39,7 @@ case $# in
             do
                 if [[ $arg1 == A* ]] && [[ $arg2 == B* ]]
                 then
-                    echo $arg1 $arg2  
+                    echo $arg1 $arg2
                     cat $arg1 $arg2 | java MatMulASCII
                 fi
             done
@@ -32,8 +48,17 @@ case $# in
     *)
         echo "ERROR: not 2 or 0 arguments"
         echo "Use 0 arguments to loop trough all the valid Matrix Multiplications in the current directory."
-        echo "Use 2 .mat files as arguments, one A?.mat and one B?.mat, either from this directory, or another,"
+        echo "Use 2 .mat files as arguments, one A?.mat and one B?.mat (where ? is a number), like theese ones:"
+        ls A*.mat
+        echo "and"
+        ls B*.mat
+        echo "You can also use files from different directories by using .. , /otherdir/ and so on..."
         ;;
 esac
 
-echo "End of program..."
+
+# Timing start counter
+let start=$(($(date +%s%N)/1000000))
+let end=$(($(date +%s%N)/1000000))
+let timing=end-start
+echo "Time to complete: $timing ms"
