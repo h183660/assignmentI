52bf5518a656f105dd8ef14b72e87c52fe3932b9
h183660 <183660@stud.hvl.no>
2021-09-22 09:31:59 +0200

    .

--- matmul.sh
+++ matmul.sh
@@ -1,82 +1,63 @@
 #! /bin/bash
-
-# Subtask 1
+# Subtask 1 Automatise compilation
 javac MatMulASCII.java
 gcc -o toBinary toBinary.c
-
-cat /dev/urandom | od | sed "s/\(.\)/\1 /g" | java MatMulASCII
-./toBinary <<< "79 107 44 32 72 101 108 108 111 10"
-
-# Subtask 2A
-
-# Current dir 
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
-
-# Filepath for the arguments
-arg1="${DIR}/${1}"
-arg2="${DIR}/${2}"
-
-# Timing start counter
-
-
-function error(){
-        echo "Use 0 arguments to loop trough all the valid Matrix Multiplications in the current directory."
-        echo "Use 2 .mat files as arguments, one A*.mat and one B*.mat (where * is a number), like theese ones:"
-        ls *.mat
+# Subtask 2 Execution and data input
+function run(){ # Function to run the java program, with a timer
+    if [ -f $arg1 ] && [ -f $arg2 ]
+    then
+        let start=$(($(date +%s%N)/1000000)) # Saves the start time in ms
+        cat $arg1 $arg2 | java MatMulASCII
+        let end=$(($(date +%s%N)/1000000)) # End time
+        let timing=end-start # Delta time 
+        echo "Time to complete the matrix multiplication of $arg1 and $arg2 was $timing ms"
+    fi
 }
-
-case $# in
+function error(){ # Error 
+    echo "Use 0 arguments to loop trough all the valid Matrix Multiplications in the current directory."
+    echo "Use 2 .mat files as arguments, one A*.mat and one B*.mat (where * is a number), like theese ones from current dir:"
+    ls *.mat
+}
+case $# in # Check number of arguments
     2)
-        if [[ $arg1 == */*A*.mat ]] && [[ $arg2 == */*B*.mat ]] && [ -f $arg1 ] && [ -f $arg2 ] # A*.mat file first arg, B*.mat file second arg, and they both exist
+        DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd) # Current dir
+        arg1=${DIR}/${1} # Adding the file exstention to the arguments
+        arg2=${DIR}/${2}
+        if [[ $arg1 == */*A*.mat ]] && [[ $arg2 == */*B*.mat ]] # A*.mat file first arg, B*.mat file second arg, and they both exist
         then
-            let start=$(($(date +%s%N)/1000000))
-            cat $arg1 $arg2 | java MatMulASCII
-            let end=$(($(date +%s%N)/1000000))
-        elif [[ $arg1 == */*B*.mat ]] && [[ $arg2 == */*A*.mat ]] && [ -f $arg1 ] && [ -f $arg2 ] # B*.mat file first arg, A*.mat file second arg, and they both exist
+            run
+        elif [[ $arg1 == */*B*.mat ]] && [[ $arg2 == */*A*.mat ]] # B*.mat file first arg, A*.mat file second arg, and they both exist
         then
-            let start=$(($(date +%s%N)/1000000))
-            cat $arg2 $arg1 | java MatMulASCII
-            let end=$(($(date +%s%N)/1000000))
-        elif [[ $1 =~ [0-9] ]] && [[ $2 =~ [0-9] ]] && [ -f A$1.mat ] && [ -f B$2.mat ] # Numbers as arguments and files exist in current directory
+            arg1=$arg2
+            arg2=${DIR}/${1}
+            run
+        elif [[ $1 =~ [0-9] ]] && [[ $2 =~ [0-9] ]] # Numbers as arguments and files exist in current directory
         then
-            let start=$(($(date +%s%N)/1000000))
-            cat A$1.mat B$2.mat | java MatMulASCII 
-            let end=$(($(date +%s%N)/1000000))
+            arg1=A$1.mat
+            arg2=B$2.mat
+            run
         else
-            echo "Invalid argument, $1 or $2 "
+            echo "Error 2: At least one invalid argument, $1 or $2 "
             error
-        fi
-
-        let timing=end-start
-        if [[ $timing -gt 0 ]]
-        then
-            echo "Time to complete the matrix multiplication of $1 and $2 was $timing ms"
         fi
         ;;
     0)
         echo "Looping through all the valid combinations of .mat files for Matrix Multiplication in current directory:"
-
-        ls *.mat # Prints every .mat file in the current directory
         set -- *.mat # Sets every .mat file as an entry in the arguments
-        let totalTime=0
-
+        let totalTime=0 # Variable to time the running of the java program
         for arg1 in $@ # Nested for loops to get every combination
         do
             for arg2 in $@
             do
                 if [[ $arg1 == A* ]] && [[ $arg2 == B* ]] # Select every valid combination for matrix multiplication
                 then
-                    echo $arg1 $arg2
-                    let start=$(($(date +%s%N)/1000000))
-                    cat $arg1 $arg2 | java MatMulASCII
-                    let end=$(($(date +%s%N)/1000000))
-                    let timing=end-star
-                    totalTime=$(($totaltime+$timing))
+                    echo "Multiplying $arg1 by $arg2"
+                    run
+                    totalTime=$((totalTime+$timing))
                 fi
             done
         done
-
-        if [[ $totalTime -gt 0 ]]
+        if [[ $totalTime -gt 0 ]] # Prints the time if it is greater than 0
         then
             echo "Time to complete all matrix multiplications from current dir is $totalTime ms"
         fi
